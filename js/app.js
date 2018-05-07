d074bf2e053740a9d8bc33d43970fffd01878a9d
Pawel Plaszczyk <pawelp73@gmail.com>
2018-05-07 13:47:21 +0200

    player movement introuced, border constraint added

--- js/app.js
+++ js/app.js
@@ -23,20 +23,57 @@ Enemy.prototype.render = function() {
 
 // Now write your own player class
 function Player(){
-this.update(),
-this.render(),
-this.handleInput(),
+this.update = function () {
+   //console.log("UPDATE");
+ };
+this.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+this.handleInput = function (movementDirect) {
+   //console.log(movementDirect);
+   switch(movementDirect) {
+    case "left":
+        if (this.x - 100 >=0){
+        this.x += -100;}
+        break;
+    case "up":
+        if (this.y - 80 >=-40){
+        this.y += -85;}
+        break;
+    case "right":
+        if (this.x + 100 <=400){
+        this.x += 100;}
+        break;
+    case "down":
+        if (this.y + 80 <=380){
+        this.y += 85;}
+        break;
+
 }
+
+ };
+this.sprite = 'images/char-boy.png';
+};
 
 // This class requires an update(), render() and
 // a handleInput() method.
 
 
 // Now instantiate your objects.
+var enemy1 = new Enemy();
+enemy1.x = 100;
+enemy1.y = 100;
+enemy1.speed =1.2;
+var enemy2 = new Enemy();
+var enemy3 = new Enemy ();
 // Place all enemy objects in an array called allEnemies
+
+var allEnemies = [enemy1, enemy2, enemy3];
 // Place the player object in a variable called player
 
-let player = new Player();
+var player = new Player();
+player.x = 200;
+player.y = 380;
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
