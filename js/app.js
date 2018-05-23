7d64fcea5297472a07cd6d4a8467f69593ee6634
Pawel Plaszczyk <pawelp73@gmail.com>
2018-05-23 17:34:45 +0200

    MORE COSMETIC CHANGES

--- js/app.js
+++ js/app.js
@@ -2,68 +2,48 @@
 to use poo instead of blood stains in places where player collided
 with bugs*/
 
-var Poo = function(){
-this.sprite = 'images/poo.png';
-this.x = 0;
-this.y = 0;
+const Poo = function() {
+  this.sprite = 'images/poo.png';
+  this.x = 0;
+  this.y = 0;
 };
-
-
-//spawn poo
-function spawnNewpoo(a,b) {
-pooinstance = new Poo();
-pooinstance.x = a;
-pooinstance.y = b;
-allPoo.push(pooinstance);
-//make player communicate
-document.getElementsByTagName("p")[2].innerHTML ="OH NO!";
-setTimeout(function(){
-document.getElementsByTagName("p")[2].innerHTML ="";
-
-}, 800);
-}
-//update courage
-/*
-function updateCourage(){
-player.courage -= 1;
-document.getElementsByTagName("span")[0].innerHTML =player.courage;
-if (player.courage == 0) {
-  player.defeat = true;
+//spawn poo where bug collided with player
+function spawnNewpoo(a, b) {
+  pooinstance = new Poo();
+  pooinstance.x = a;
+  pooinstance.y = b;
+  allPoo.push(pooinstance);
+  //communicate 'OH NO' to the player
+  document.getElementsByTagName("p")[2].innerHTML = "OH NO!";
+  setTimeout(function() {
+    document.getElementsByTagName("p")[2].innerHTML = "";
+  }, 800);
 }
 
-}
-*/
-function restartGame(){
+function restartGame() {
   player.x = 202;
-  player.y =380;
-  player.courage =4;
+  player.y = 380;
+  player.courage = 4;
   player.updateCourage();
   player.keyinputOn = true;
   player.victory = false;
   player.defeat = false;
-  allPoo =[];
-  document.getElementsByTagName("p")[3].innerHTML ="";
-  document.getElementsByTagName("p")[2].innerHTML ="";
+  allPoo = [];
+  allEnemies.forEach(function(enemy) {
+    enemy.spawnEnemy();
+  });
+  document.getElementsByTagName("p")[3].innerHTML = "";
+  document.getElementsByTagName("p")[2].innerHTML = "";
 }
 
 Poo.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-
-
-
-// Enemies our player must avoid
-var Enemy = function() {
-  // Variables applied to each of our instances go here,
-  // we've provided one for you to get started
-
-  // The image/sprite for our enemies, this uses
-  // a helper we've provided to easily load images
+// Enemies
+const Enemy = function() {
   this.sprite = 'images/enemy-bug.png';
-  //this.hitbox = [this.x, this.x +101, this.y +75, this.y +145];
 };
-
 
 Enemy.prototype.spawnEnemy = function() {
   this.random = function(number) {
@@ -76,37 +56,12 @@ Enemy.prototype.spawnEnemy = function() {
   this.y = 60 + (this.line - 1) * 83;
 }
 
-/*
-Enemy.prototype.checkCollision(){
-if (player.y >=48 && player.y <=214){
-  if ((this.x+ 91)> player.x && this.x < (player.x +91) && (this.y === player.y +12)){
-  console.log ("ouch");
-  player.playerDied();
-
-}
-*/
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-  // You should multiply any movement by the dt parameter
-
-
-
-  // which will ensure the game runs at the same speed for
-  // all computers.
   this.x += 120 * dt * this.speed;
-  //console.log(dt);
-  //check board limits
+  //if enemy out of the board spawn it again
   if (this.x > 510) {
     this.spawnEnemy();
-    //console.log('chuj');
-    //spawnEneemy();
-};
-
-
-
-
-
+  };
 };
 
 // Draw the enemy on the screen, required method for game
@@ -117,148 +72,91 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 function Player() {
   this.update = function() {
-    //console.log("UPDATE");
-    /*checkcollision -initial condition ommits safe zone
-    no need to check and slow down processing*/
-    //CHECK PLAYER VICTORY
-    if (this.y == -35){
-    console.log('vic');
-    let victory = true;
-    let tar = document.getElementsByTagName("p")[0];
-    tar.innerHTML = 'You have won! Congratulations!';
-    //console.log(x);
-    player.victory = true;
-    }
-
-
-    if (this.y >=48 && this.y <=214){
+    if (this.y == -35) {
+      player.victory = true;
+    } else if (this.y >= 48 && this.y <= 214) {
 
       allEnemies.forEach(function(enemy) {
-      //10 pixels are cut from PNG to make player hitbox lower
-      if ((enemy.x+ 91)> player.x && enemy.x < (player.x +91) && (enemy.y === player.y +12))
-      {
-      //PLAYER COLLISION DETECTED!
-      console.log ("ouch");
-      //spawn new poo in place of unfortunate accident
-      spawnNewpoo(player.x, player.y+20);
-      //update courage and check if the game is not lost
-      player.updateCourage();
-      //pooinstance = new Poo();
-      //pooinstance.x = player.x;
-      //pooinstance.y = player.y+20;
-      //allPoo.push(pooinstance);
-
-      setTimeout(function(){
-      pooinstance.immune = false;
-
-      }, 300);
-      player.x = 202;
-      player.y = 380;
-
-
-
-      //check for poo collision now!
-
-
-      //console.log(poo1.x);
-      //console.log(poo1.y);
-      //allPoo.push("poo2");
-      //console.log (allPoo);
-    }
-
-
+        //adjusting hit boxes
+        if ((enemy.x + 91) > player.x && enemy.x < (player.x + 91) && (enemy.y === player.y + 12)) {
+          //PLAYER COLLISION DETECTED!
+          spawnNewpoo(player.x, player.y + 20);
+          player.updateCourage();
+          player.x = 202;
+          player.y = 380;
+        }
       });
     }
-
   };
   this.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   };
   this.handleInput = function(movementDirect) {
-    //console.log(movementDirect);
-
-if (player.keyinputOn){
-    switch (movementDirect) {
-      case "left":
-        if (this.x - 101 >= -51) {
-          this.x += -101;
-        }
-        break;
-      case "up":
-        if (this.y - 83 >= -42) {
-          this.y += -83;
-        }
-        break;
-      case "right":
-        if (this.x + 101 <= 404) {
-          this.x += 101;
-        }
-        break;
-      case "down":
-        if (this.y + 83 <= 412) {
-          this.y += 83;
-        }
-        break;
-
+    if (player.keyinputOn) {
+      switch (movementDirect) {
+        case "left":
+          if (this.x - 101 >= -51) {
+            this.x += -101;
+          }
+          break;
+        case "up":
+          if (this.y - 83 >= -42) {
+            this.y += -83;
+          }
+          break;
+        case "right":
+          if (this.x + 101 <= 404) {
+            this.x += 101;
+          }
+          break;
+        case "down":
+          if (this.y + 83 <= 412) {
+            this.y += 83;
+          }
+          break;
+      }
     }
-  }
   };
   this.sprite = 'images/char-boy.png';
-
-  //define hitbox so it ignores PNG.'s whitespace
-  this.hitbox = [this.x+15, this.x +86, this.y +50, this.y +151];
   //player's life
-  this.courage =3;
+  this.courage = 3;
   this.victory = false;
   this.defeat = false;
+  //false keyintputOn blocks player movement after the game
   this.keyinputOn = true;
-  this.updateCourage = function(){
-  this.courage -= 1;
-  document.getElementsByTagName("span")[0].innerHTML = this.courage;
-    if (this.courage == 0) {
+  this.x = 202;
+  this.y = 380;
+  this.updateCourage = function() {
+    this.courage -= 1;
+    document.getElementsByTagName("span")[0].innerHTML = this.courage;
+    if (!this.courage) {
       this.defeat = true;
     }
-
-
   }
 };
 
-// This class requires an update(), render() and
-// a handleInput() method.
+// 3 bugs should be enough
+let allEnemies = [];
+while (allEnemies.length <3){
+let enemybug = new Enemy();
+enemybug.spawnEnemy();
+allEnemies.push(enemybug);
+}
 
-
-// Now instantiate your objects.
-let enemy1 = new Enemy();
-enemy1.spawnEnemy();
-let enemy2 = new Enemy();
-enemy2.spawnEnemy();
-let enemy3 = new Enemy();
-enemy3.spawnEnemy();
-//let poo1 = new Poo();
-//let poo2 = new Poo();
-//let poo3 = new Poo();
-
-// Place all enemy objects in an array called allEnemies
-var allPoo =[];
-var allEnemies = [enemy1, enemy2, enemy3];
+let allPoo = [];
 // Place the player object in a variable called player
+let player = new Player();
 
-var player = new Player();
-player.x = 202;
-player.y = 380;
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+//addition: enter key is listened for so a new game can start
 document.addEventListener('keyup', function(e) {
-  var allowedKeys = {
+  let allowedKeys = {
     37: 'left',
     38: 'up',
     39: 'right',
     13: 'enter',
     40: 'down'
   };
-  if (allowedKeys[e.keyCode] == "enter" && (player.victory || player.defeat)){
-    console.log ("alejazda");
+  if (allowedKeys[e.keyCode] == "enter" && (player.victory || player.defeat)) {
     restartGame();
   }
   player.handleInput(allowedKeys[e.keyCode]);
--- js/engine.js
+++ js/engine.js
@@ -1,247 +1,107 @@
-/* Engine.js
- * This file provides the game loop functionality (update entities and render),
- * draws the initial game board on the screen, and then calls the update and
- * render methods on your player and enemy objects (defined in your app.js).
- *
- * A game engine works by drawing the entire game screen over and over, kind of
- * like a flipbook you may have created as a kid. When your player moves across
- * the screen, it may look like just that image/character is moving or being
- * drawn but that is not the case. What's really happening is the entire "scene"
- * is being drawn over and over, presenting the illusion of animation.
- *
- * This engine makes the canvas' context (ctx) object globally available to make
- * writing app.js a little simpler to work with.
- */
-
 let Engine = (function(global) {
-    /* Predefine the variables we'll be using within this scope,
-     * create the canvas element, grab the 2D context for that canvas
-     * set the canvas elements height/width and add it to the DOM.
-     */
-    let doc = global.document,
-        win = global.window,
-        canvas = doc.createElement('canvas'),
-        ctx = canvas.getContext('2d'),
-        lastTime,
-        courage = 3,
-        node = doc.createElement("P"),
-        node2 = doc.createElement("P"),
-        node3 = doc.createElement("P"),
-        node4 = doc.createElement("P"),
-        textnode = doc.createTextNode("Conquer your fear and get to the water!");
 
+  let doc = global.document,
+    win = global.window,
+    canvas = doc.createElement('canvas'),
+    ctx = canvas.getContext('2d'),
+    lastTime,
+    node = doc.createElement("P"),
+    node2 = doc.createElement("P"),
+    node3 = doc.createElement("P"),
+    node4 = doc.createElement("P"),
+    textnode = doc.createTextNode("Conquer your fear and get to the water!");
 
-    canvas.width = 505;
-    canvas.height = 606;
+  canvas.width = 505;
+  canvas.height = 606;
 
-    node.appendChild(textnode);
+  node.appendChild(textnode);
+  doc.body.appendChild(node);
+  doc.body.appendChild(node2);
+  doc.body.appendChild(canvas);
+  doc.body.appendChild(node3);
+  doc.getElementsByTagName("p")[1].innerHTML = `Courage:  <span class = "counter-background">${player.courage}</span>`;
+  doc.body.appendChild(node4);
 
-    //node2.appendChild(textnode2);
-    doc.body.appendChild(node);
-    doc.body.appendChild(node2);
-    doc.body.appendChild(canvas);
-    doc.body.appendChild(node3);
-    doc.getElementsByTagName("p")[1].innerHTML =`Courage:  <span class = "counter-background">${player.courage}</span>`;
-    doc.body.appendChild(node4);
+  function main() {
 
-    /* This function serves as the kickoff point for the game loop itself
-     * and handles properly calling the update and render methods.
-     */
-    function main() {
-        /* Get our time delta information which is required if your game
-         * requires smooth animation. Because everyone's computer processes
-         * instructions at different speeds we need a constant value that
-         * would be the same for everyone (regardless of how fast their
-         * computer is) - hurray time!
-         */
-        var now = Date.now(),
-            dt = (now - lastTime) / 1000.0;
-
-        /* Call our update/render functions, pass along the time delta to
-         * our update function since it may be used for smooth animation.
-         */
-         if (player.defeat){
-         doc.getElementsByTagName("p")[2].innerHTML ="YOU HAVE LOST!";
-         player.keyinputOn = false;
-         promptNew();
-
-         }
-         if (player.victory){
-         doc.getElementsByTagName("p")[2].innerHTML ="YOU HAVE WON!<br>CONGRATULATIONS!";
-         player.keyinputOn = false;
-         promptNew();
-         }
-
-        function promptNew(){
-        doc.getElementsByTagName("p")[3].innerHTML ="PRESS <span class ='brick'>ENTER</span> TO START NEW GAME";
-        //document.addEventListener('keyup', function(){
-        //document.onkeypress = function(){
-        //let x = event.key;
-        //console.log(x);
-        //if (x =="Enter"){
-        //console.log('chuj');
-
-        //player.victory = false;
-        //player.defeat =false;
-        //player.keyinputOn = true;
-
-        //allEnemies.forEach(function(enemy){
-        //enemy.spawnEnemy();
-        //});
-
-        //document.removeEventListener("onkeypress",);
-        //}
-        };
-
-        update(dt);
-        render();
-
-        /* Set our lastTime variable which is used to determine the time delta
-         * for the next time this function is called.
-         */
-        lastTime = now;
-
-        /* Use the browser's requestAnimationFrame function to call this
-         * function again as soon as the browser is able to draw another frame.
-         */
-        win.requestAnimationFrame(main);
+    function promptNew() {
+      doc.getElementsByTagName("p")[3].innerHTML = "PRESS <span class ='brick'>ENTER</span> TO START NEW GAME";
+    };
+    let now = Date.now(),
+      dt = (now - lastTime) / 1000.0;
+    //player object signalled defeat or victory - stop player input and communicate status
+    if (player.defeat) {
+      doc.getElementsByTagName("p")[2].innerHTML = "YOU HAVE LOST!";
+      player.keyinputOn = false;
+      promptNew();
+    } else if (player.victory) {
+      doc.getElementsByTagName("p")[2].innerHTML = "YOU HAVE WON!<br>CONGRATULATIONS!";
+      player.keyinputOn = false;
+      promptNew();
     }
 
-    /* This function does some initial setup that should only occur once,
-     * particularly setting the lastTime variable that is required for the
-     * game loop.
-     */
-    function init() {
-        reset();
-        lastTime = Date.now();
-        main();
+    update(dt);
+    render();
+    lastTime = now;
+    win.requestAnimationFrame(main);
+  }
+
+  function init() {
+    lastTime = Date.now();
+    main();
+  }
+  function update(dt) {
+    updateEntities(dt);
+  }
+  function updateEntities(dt) {
+    allEnemies.forEach(function(enemy) {
+      enemy.update(dt);
+    });
+    player.update();
+  }
+
+  function render() {
+
+    let rowImages = [
+        'images/water-block.png', // Top row is water
+        'images/stone-block.png', // Row 1 of 3 of stone
+        'images/stone-block.png', // Row 2 of 3 of stone
+        'images/stone-block.png', // Row 3 of 3 of stone
+        'images/grass-block.png', // Row 1 of 2 of grass
+        'images/grass-block.png' // Row 2 of 2 of grass
+      ],
+      numRows = 6,
+      numCols = 5,
+      row, col;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    for (row = 0; row < numRows; row++) {
+      for (col = 0; col < numCols; col++) {
+        ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+      }
     }
+    renderEntities();
+  }
+  function renderEntities() {
+    allPoo.forEach(function(poo) {
+      poo.render();
+    });
+    allEnemies.forEach(function(enemy) {
+      enemy.render();
+    });
+    player.render();
+  }
 
-    /* This function is called by main (our game loop) and itself calls all
-     * of the functions which may need to update entity's data. Based on how
-     * you implement your collision detection (when two entities occupy the
-     * same space, for instance when your character should die), you may find
-     * the need to add an additional function call here. For now, we've left
-     * it commented out - you may or may not want to implement this
-     * functionality this way (you could just implement collision detection
-     * on the entities themselves within your app.js file).
-     */
+  Resources.load([
+    'images/stone-block.png',
+    'images/water-block.png',
+    'images/grass-block.png',
+    'images/enemy-bug.png',
+    'images/char-boy.png',
+    'images/poo.png'
+  ]);
+  Resources.onReady(init);
 
-    function update(dt) {
-        updateEntities(dt);
-        // checkCollisions();
-    }
-
-    /* This is called by the update function and loops through all of the
-     * objects within your allEnemies array as defined in app.js and calls
-     * their update() methods. It will then call the update function for your
-     * player object. These update methods should focus purely on updating
-     * the data/properties related to the object. Do your drawing in your
-     * render methods.
-     */
-    function updateEntities(dt) {
-        allEnemies.forEach(function(enemy) {
-            enemy.update(dt);
-        });
-        player.update();
-    }
-
-    /* This function initially draws the "game level", it will then call
-     * the renderEntities function. Remember, this function is called every
-     * game tick (or loop of the game engine) because that's how games work -
-     * they are flipbooks creating the illusion of animation but in reality
-     * they are just drawing the entire screen over and over.
-     */
-    function render() {
-        /* This array holds the relative URL to the image used
-         * for that particular row of the game level.
-         */
-        var rowImages = [
-                'images/water-block.png',   // Top row is water
-                'images/stone-block.png',   // Row 1 of 3 of stone
-                'images/stone-block.png',   // Row 2 of 3 of stone
-                'images/stone-block.png',   // Row 3 of 3 of stone
-                'images/grass-block.png',   // Row 1 of 2 of grass
-                'images/grass-block.png'    // Row 2 of 2 of grass
-            ],
-            numRows = 6,
-            numCols = 5,
-            row, col;
-
-        // Before drawing, clear existing canvas
-        ctx.clearRect(0,0,canvas.width,canvas.height)
-
-        /* Loop through the number of rows and columns we've defined above
-         * and, using the rowImages array, draw the correct image for that
-         * portion of the "grid"
-         */
-        for (row = 0; row < numRows; row++) {
-            for (col = 0; col < numCols; col++) {
-                /* The drawImage function of the canvas' context element
-                 * requires 3 parameters: the image to draw, the x coordinate
-                 * to start drawing and the y coordinate to start drawing.
-                 * We're using our Resources helpers to refer to our images
-                 * so that we get the benefits of caching these images, since
-                 * we're using them over and over.
-                 */
-                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
-            }
-        }
-
-        renderEntities();
-    }
-
-    /* This function is called by the render function and is called on each game
-     * tick. Its purpose is to then call the render functions you have defined
-     * on your enemy and player entities within app.js
-     */
-    function renderEntities() {
-        /* Loop through all of the objects within the allEnemies array and call
-         * the render function you have defined.
-         */
-         allPoo.forEach(function(poo) {
-           poo.render();
-         });
-
-        allEnemies.forEach(function(enemy) {
-            enemy.render();
-        });
-
-        player.render();
-        //poo must also be rendered!
-
-
-
-
-    }
-
-    /* This function does nothing but it could have been a good place to
-     * handle game reset states - maybe a new game menu or a game over screen
-     * those sorts of things. It's only called once by the init() method.
-     */
-    function reset() {
-        // noop
-        console.log("RESET");
-    }
-
-    /* Go ahead and load all of the images we know we're going to need to
-     * draw our game level. Then set init as the callback method, so that when
-     * all of these images are properly loaded our game will start.
-     */
-    Resources.load([
-        'images/stone-block.png',
-        'images/water-block.png',
-        'images/grass-block.png',
-        'images/enemy-bug.png',
-        'images/char-boy.png',
-        'images/poo.png',
-        'images/poo2.png'
-    ]);
-    Resources.onReady(init);
-
-    /* Assign the canvas' context object to the global variable (the window
-     * object when run in a browser) so that developers can use it more easily
-     * from within their app.js files.
-     */
-    global.ctx = ctx;
+  global.ctx = ctx;
 })(this);
