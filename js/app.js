8573ee004816666353256712fb4f07d50fc13560
Pawel Plaszczyk <pawelp73@gmail.com>
2018-05-24 18:38:25 +0200

    STRICT MODE ON FOR THE WHOLE FILE

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
+'use strict';
 /*Poo seems to be more age-neutral than blood therefore I decided
 to use poo instead of blood stains in places where player collided
 with bugs*/
-'use strict';
 const Poo = function() {
   this.sprite = "images/poo.png";
   this.x = 0;
